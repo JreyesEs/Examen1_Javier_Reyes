66186afc274482f3051c2e1e7e24f75d0c137cf9
JreyesEs <115844060+JreyesEs@users.noreply.github.com>
2024-02-15 17:32:17 -0600

    Add files via upload

---
+++ Examen1_Javier_Reyes/src/examen1_javier_reyes/Examen1_Javier_Reyes.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen1_javier_reyes;
+import java.util.Scanner;
+/**
+ *
+ * @author Javier Reyes
+ */
+public class Examen1_Javier_Reyes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int menu;
+        
+        
+        do{
+            System.out.println("Menu del primer examen de programacion: ");
+            System.out.println("1. Ejercicio String");
+            System.out.println("2. Respuestas a preguntas");
+            System.out.println("3. Mostrar el historial del menu");
+            System.out.println("4. Salir");
+            System.out.print("Escoja una opcion: ");
+            menu = entrada.nextInt();
+
+            System.out.println("");
+            switch (menu) {
+                case 1:
+                    System.out.println("");
+                    
+                    break;
+                    
+                case 2:
+                    
+                    
+                    break;
+                
+                
+                case 3:
+                    
+                    
+                    break;
+            
+            }
+            
+        }while (menu != 4);
+        
+        
+        // TODO code application logic here
+    }
+    
+}
